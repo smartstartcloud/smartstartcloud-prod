fdbca0bceb3c4f5efee99b175d91ef171df964d6
farhan1611078 <1611078@iub.edu.bd>
2024-09-04 15:19:00 +0600

    login with jwt cookie

---
+++ backend/controllers/login.js
@@ -0,0 +1,52 @@
+import express from 'express'
+const app = express.Router()
+import bcrypt from 'bcryptjs'
+import User from '../models/user.models.js'
+import jwt from 'jsonwebtoken'
+
+
+function createToken(username){
+    return jwt.sign({username},process.env.JWT_KEY,{expiresIn:5*60*1000}); //in minute
+}
+
+app.get("/",async (req,res)=>{
+    // check if there is a msg query
+    let bad_auth = req.query.msg ? true : false;
+
+    // if there exists, send the error.
+    if (bad_auth) {
+        return res.render("login", {
+        error: "Invalid username or password",
+        });
+    } else {
+        // else just render the login
+        return res.render("login");
+    }
+})
+
+app.post('/',async(req,res)=>{
+    try {
+    const user = await User.findOne({ userName: req.body.username }); 
+    if (user) {
+        await bcrypt.compare( req.body.password, user.password, function(err, result) {
+            if (result) {
+                const token = createToken(user.userName);
+                res.cookie('jwt',token,{httpOnly: true, maxAge:5*60*1000}); //in minute
+                res.sendStatus(200);
+                //Testing UI code
+                //res.render('dashboard',{name: user.userName}) 
+                } else {
+                    //return res.redirect("/login?msg=fail");
+                    return res.status(400).json({error: "Invalid Username or Password"});
+                }
+        });
+    } else {
+        //return res.redirect("/login?msg=fail");
+        return res.status(400).json({error: "Invalid Username or Password"});
+    }
+    } catch (error) {
+    console.log(error);
+    res.status(500).send("Internal Server error Occured");
+    }
+})
+export default app;
--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,28 @@
 import dotenv from 'dotenv'
 import express from 'express'
 import authRoutes from './routes/auth.routes.js'
+import loginRoute from "./controllers/login.js"
 import connectToMongoDB from './db/connectToMongoDB.js'
 
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 // express app
 const app = express()
 dotenv.config()
 
-app.use(express.json()) // Parse from req body
+app.set('view engine','ejs');
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+app.use(express.static(__dirname + "/public"));
 
-app.use("/api/auth", authRoutes)
+app.use("/api/auth", authRoutes);
+app.use("/login", loginRoute);
 
 app.listen(process.env.PORT, () => {
-    console.log('listening on port 4000');
+    console.log(`listening on port ${process.env.PORT}`);
     connectToMongoDB()
 
 })
