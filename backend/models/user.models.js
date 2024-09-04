f7e8910bc972c85f2d52354092d45df4d404611e
shiammisbah <shiam.misbah42@gmail.com>
2024-09-04 06:42:14 +0600

    Backend Created

---
+++ backend/models/user.models.js
@@ -0,0 +1,35 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    firstName: {
+        type: String,
+        required: true
+    },
+    lastName: {
+        type: String,
+        required: true
+    },
+    userName: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 6
+    },
+    gender: {
+        type: String,
+        required: true,
+        enum: ["male", "female", "other"]
+    }
+})
+
+const User = mongoose.model("User", userSchema)
+
+export default User;
