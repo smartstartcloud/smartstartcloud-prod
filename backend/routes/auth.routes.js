df296e550cfccbd2df48ae923069fd00c8bc3750
farhantech50 <farhantech50@gmail.com>
2024-09-04 15:19:00 +0600

    login with jwt cookie

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -1,12 +1,10 @@
-import express from "express";
-import { signupUser, loginUser, logoutUser } from "../controllers/auth.controller.js";
+import express from "express"
+import { loginUser, signupUser, logoutUser } from "../controllers/auth.controller.js"
+
 
 const router = express.Router();
 
 router.post("/signup", signupUser)
-
-router.post("/login", loginUser)
-
 router.post("/logout", logoutUser)
 
 export default router;
