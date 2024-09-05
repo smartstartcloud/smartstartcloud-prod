113df73bf6b007eebe5162643c28333f80f1387c
shiammisbah <shiam.misbah42@gmail.com>
2024-09-05 07:28:53 +0600

    login logout api created

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -5,6 +5,7 @@ import { loginUser, signupUser, logoutUser } from "../controllers/auth.controlle
 const router = express.Router();
 
 router.post("/signup", signupUser)
+router.post("/login", loginUser)
 router.post("/logout", logoutUser)
 
 export default router;
