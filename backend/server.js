3a2b2e221317965fdf532237275d1872baa89bcd
farhantech50 <farhantech50@gmail.com>
2024-09-05 22:05:12 +0600

    u

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,7 @@ app.use(express.json());
 app.use(express.static(__dirname + "/public"));
 
 app.use("/api/auth", authRoutes);
-// app.use("/login", loginRoute);
+app.use("/login", loginRoute);
 app.use("/dummyRequest", protect,dummyRequestRoute); //USE POSTMAN TO CHECK
 
 app.listen(process.env.PORT, () => {
