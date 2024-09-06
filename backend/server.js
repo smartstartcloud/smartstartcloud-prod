ca620803825f87d4e7065c64f2281ed9b09a9819
farhantech50 <farhantech50@gmail.com>
2024-09-07 01:17:01 +0600

    working cookie in client side

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,10 @@ dotenv.config()
 app.set('view engine','ejs');
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
-app.use(cors());
+app.use((cors({
+    origin: true,  // Or set to true for dynamic origin
+    credentials: true
+})));
 app.use(express.static(__dirname + "/public"));
 
 app.use("/api/auth", authRoutes);
