1b1994512d35f9a50e8b4165ef621a0751904522
shiammisbah <shiam.misbah42@gmail.com>
2024-09-07 02:39:58 +0600

    Token accessed in FE

--- Dashboard/src/scenes/dashboard/index.jsx
+++ Dashboard/src/scenes/dashboard/index.jsx
@@ -1,15 +1,37 @@
 import { Box } from '@mui/material'
 import React from 'react'
 import Header from '../../components/Header'
+import { useTokenContext } from '../../context/TokenContext'
 
 const Dashboard = () => {
-  return (
-    <Box m="20px">
-      <Box display="flex" justifyContent="space-between" alignItems="center">
-        <Header title={"DASHBOARD"} subtitle={"Welcome to Dashboard"} />
+    const {accessToken} = useTokenContext()
+
+    const handleToken = async () => {
+      if (!accessToken) {
+        alert('You need to login first!');
+        return;
+      }
+
+      const res = await fetch('http://localhost:5000/dummyRequest', {
+        method: 'GET',
+        headers: { 'Authorization': `Bearer ${accessToken}` }
+      });
+
+      const data = await res.json();
+      console.log(data);
+      
+    }
+    
+    
+    return (
+      <Box m="20px">
+        <Box display="flex" justifyContent="space-between" alignItems="center">
+          <Header title={"DASHBOARD"} subtitle={"Welcome to Dashboard"} />
+        </Box>
+        <h1>Make a Protected Request</h1>
+        <button onClick={handleToken}>Access Protected Route</button>
       </Box>
-    </Box>
-  )
+    )
 }
 
 export default Dashboard
