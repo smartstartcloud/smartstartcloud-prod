d0bff72d4b9ce818ea6b3ad375d8f68036e11ef5
farhantech50 <farhantech50@gmail.com>
2024-09-13 03:38:32 +0600

    Cluster Backend

--- backend/server.js
+++ backend/server.js
@@ -5,24 +5,40 @@ import dummyRequestRoute from "./controllers/dummyRequest.js"
 import protect from './middlewares/protect.js'
 import cors from 'cors'
 import cookieParser from 'cookie-parser'
+import cluster from 'cluster'
+import cpu from 'os'
 
-// express app
-const app = express()
-app.use(cookieParser());
+const totalCPUs = cpu.cpus().length;
+const numWorkers = process.env.WEB_CONCURRENCY || totalCPUs ;
 
-app.set('view engine','ejs');
-app.use(express.urlencoded({extended: true}));
-app.use(express.json());
-app.use((cors({
-    origin: true,  // Or set to true for dynamic origin
-    credentials: true
-})));
+if(cluster.isPrimary) {
+  for (var i = 0; i < numWorkers; i++) {
+    cluster.fork();
+  }
+  cluster.on('exit', function() {
+    cluster.fork();
+  });
 
-app.use("/api/auth", authRoutes);
-app.use("/dummyRequest", protect,dummyRequestRoute); //USE POSTMAN TO CHECK
+} else {
+    const app = express()
+    app.use(cookieParser());
+
+    app.set('view engine','ejs');
+    app.use(express.urlencoded({extended: true}));
+    app.use(express.json());
+    app.use((cors({
+        origin: true,  // Or set to true for dynamic origin
+        credentials: true
+    })));
+
+    app.use("/api/auth", authRoutes);
+    app.use("/dummyRequest", protect,dummyRequestRoute); //USE POSTMAN TO CHECK
+
+    app.listen(process.env.PORT, () => {
+        console.log(`listening on port ${process.env.PORT}`);
+    })
+}
 
 
-app.listen(process.env.PORT, () => {
-    console.log(`listening on port ${process.env.PORT}`);
-})
+
 
