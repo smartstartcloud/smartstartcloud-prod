fd88598465f6ffae6fc8dc70fa8dcb0642a5a183
farhantech50 <farhantech50@gmail.com>
2024-09-13 15:08:01 +0600

    update

---
+++ Backend/db/connectMongoDB.js
@@ -0,0 +1,27 @@
+import mongo from 'mongoose';
+
+const connectMongoAuth = async ()=>{
+    try {
+        const db = await mongo.createConnection(process.env.MONGO_DB_URI_AUTH);
+        db.on(`error`, console.error.bind(console, `connection error:`));
+        db.once(`open`, function () {console.log(`MongoDB connected on AUTH`);});
+        return db;
+        
+    } catch (error) {
+        console.log("Error connecting Database", error);
+    }
+}
+const connectMongoDegree = async ()=>{
+    try {
+        const db = await mongo.createConnection(process.env.MONGO_DB_URI_DEGREE);
+        db.on(`error`, console.error.bind(console, `connection error:`));
+        db.once(`open`, function () {console.log(`MongoDB connected on DEGREE`);});
+        return db;
+        
+    } catch (error) {
+        console.log("Error connecting Database", error);
+    }
+}
+
+
+export {connectMongoAuth,connectMongoDegree};
---
+++ Backend/server.js
@@ -0,0 +1,44 @@
+import dotenv from './utils/env.js'
+import express from 'express'
+import authRoutes from './routes/auth.routes.js'
+import dummyRequestRoute from "./controllers/dummyRequest.js"
+import protect from './middlewares/protect.js'
+import cors from 'cors'
+import cookieParser from 'cookie-parser'
+import cluster from 'cluster'
+import cpu from 'os'
+import './models/degree.models.js'
+import './models/user.models.js'
+
+const totalCPUs = cpu.cpus().length;
+const numWorkers = process.env.WEB_CONCURRENCY || totalCPUs ;
+
+if(cluster.isPrimary) {
+  for (var i = 0; i < numWorkers; i++) {
+    cluster.fork();
+  }
+  cluster.on('exit', function() {
+    cluster.fork();
+  });
+
+} else {
+    // express app
+    const app = express()
+    app.use(cookieParser());
+
+    app.set('view engine','ejs');
+    app.use(express.urlencoded({extended: true}));
+    app.use(express.json());
+    app.use((cors({
+        origin: true,  // Or set to true for dynamic origin
+        credentials: true
+    })));
+
+    app.use("/api/auth", authRoutes);
+    app.use("/dummyRequest", protect,dummyRequestRoute); //USE POSTMAN TO CHECK
+
+
+    app.listen(process.env.PORT, () => {
+        console.log(`listening on port ${process.env.PORT}`);
+    })
+}
