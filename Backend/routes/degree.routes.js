b0083d23dd0fec6c7348a2845bd8351560d4672d
ifransaad <ifran.saad100@gmail.com>
2024-09-18 08:38:35 +0600

    Update degree.route.js

--- Backend/routes/degree.routes.js
+++ Backend/routes/degree.routes.js
@@ -1,8 +1,20 @@
 import express from "express"
 import {newDegree} from '../controllers/degree.controller.js'
+import Degree from '../models/degree.models.js';
 
 const router = express.Router();
 
 router.post("/new", newDegree)
+
+// GET route to fetch all degrees
+router.get('/degrees', async (req, res) => {
+    try {
+      const degrees = await Degree.find({});
+      res.status(200).json(degrees);
+    } catch (error) {
+      console.error("Error fetching degrees:", error);
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
+  });
 
 export default router;
