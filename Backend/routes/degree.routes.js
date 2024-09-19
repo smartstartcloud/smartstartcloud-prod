4ee32b7fa5f034977e2ba218a22630dfff2ef9b8
farhantech50 <farhantech50@gmail.com>
2024-09-19 18:40:36 +0600

    update

--- Backend/routes/degree.routes.js
+++ Backend/routes/degree.routes.js
@@ -1,10 +1,15 @@
 import express from "express"
-import {newDegree,getDegree} from '../controllers/degree.controller.js'
+import {newDegree,getDegree, getSelectedDegree, getSingleDegree} from '../controllers/degree.controller.js'
 
 const router = express.Router();
 
 router.post("/new",newDegree);
 
 router.get('/all',getDegree) ;
+
+router.get('/selected/year/:degreeYear',getSelectedDegree) ;
+
+router.get('/selected/id/:degreeID',getSingleDegree) ;
+
 
 export default router;
