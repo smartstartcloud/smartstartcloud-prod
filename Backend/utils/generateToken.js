cf01fb58b7fe2bb8304494c78f8068f0975a1c34
farhantech50 <farhantech50@gmail.com>
2024-09-26 01:17:13 +0600

    update

--- Backend/utils/generateToken.js
+++ Backend/utils/generateToken.js
@@ -3,13 +3,13 @@ const app = express.Router()
 import jwt from 'jsonwebtoken'
 
 const generateAccessToken = (userId,userRole) => {
-    const token = jwt.sign({userId,userRole},process.env.JWT_KEY,{expiresIn:"5s"})
+    const token = jwt.sign({userId,userRole},process.env.JWT_KEY,{expiresIn:"10s"})
     return token
 };
 
 
 const generateRefreshToken = (userId,userRole,res) => {
-    const token = jwt.sign({userId,userRole},process.env.JWT_KEY,{expiresIn:"10s"})
+    const token = jwt.sign({userId,userRole},process.env.JWT_KEY,{expiresIn:"30s"})
     res.cookie('refreshToken',token,{httpOnly: true, maxAge: 24*60*60*1000, secure: true, sameSite: "strict"}) 
 };
 
@@ -19,7 +19,7 @@ const newAccessToken = app.post("/",async (req,res)=>{
     if (!refreshToken) return res.status(403).json({"error": "Forbidden in token Route", "message": "No Refresh Token"});  
   
     jwt.verify(refreshToken, process.env.JWT_KEY, (err, user) => {
-      if (err) return res.status(404).json({"error": "Forbidden in token Route", "message": "You do not have permission to access this resource."});
+      if (err) return res.status(403).json({"error": "Forbidden in token Route", "message": "You do not have permission to access this resource."});
       // `user` contains the decoded data (e.g., userId, roles)
       const userId = user.userId; // Extract userId from the token
       const newAccessToken = generateAccessToken(userId);
