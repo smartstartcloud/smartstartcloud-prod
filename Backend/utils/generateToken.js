05725e9efbb1569dcab276eb371f52f26f69a420
farhantech50 <farhantech50@gmail.com>
2024-09-27 05:37:31 +0600

    update

--- Backend/utils/generateToken.js
+++ Backend/utils/generateToken.js
@@ -3,14 +3,14 @@ const app = express.Router()
 import jwt from 'jsonwebtoken'
 
 const generateAccessToken = (userId,userRole) => {
-    const token = jwt.sign({userId,userRole},process.env.JWT_KEY,{expiresIn:"10s"})
+    const token = jwt.sign({userId,userRole},process.env.JWT_KEY,{expiresIn:"30s"})
     return token
 };
 
 
 const generateRefreshToken = (userId,userRole,res) => {
     const token = jwt.sign({userId,userRole},process.env.JWT_KEY,{expiresIn:"1h"})
-    res.cookie('refreshToken',token,{httpOnly: true, maxAge: 24*60*60*1000, secure: true, sameSite: "strict"}) 
+    res.cookie('refreshToken',token,{httpOnly: true, maxAge: 24*60*60*1000, secure: true, sameSite: "None"}) 
 };
 
 const newAccessToken = app.post("/",async (req,res)=>{
