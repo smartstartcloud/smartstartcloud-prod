ef9b95ef11268d78f323b1c053cfd66007928889
ifransaad <ifran.saad100@gmail.com>
2024-09-26 10:08:13 +0600

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -54,10 +54,13 @@ app.use(cookieParser());
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use(helmet());
-app.use((cors({
-    origin: true,  // Or set to true for dynamic origin
-    credentials: true
-})));
+const cors = require('cors');
+const corsOptions ={
+  origin:'http://localhost:3000', 
+  credentials:true,            //access-control-allow-credentials:true
+  optionSuccessStatus:200
+}
+app.use(cors(corsOptions));
 
 app.use("/api/auth", authRoutes);
 app.use("/api/degree", degreeRoutes);
