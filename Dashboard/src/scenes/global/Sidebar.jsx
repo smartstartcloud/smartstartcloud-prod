3aa2ad8927070a4da93c73354e015a8a49659622
ifransaad <ifran.saad100@gmail.com>
2024-09-28 11:01:59 +0600

    Updated Sidebar with Upload test page

--- Dashboard/src/scenes/global/Sidebar.jsx
+++ Dashboard/src/scenes/global/Sidebar.jsx
@@ -164,6 +164,13 @@ const Sidebar = () => {
                 selected={selected}
                 setSelected={setSelected}
               />
+              <Item 
+                title="Upload Test"
+                to="/testpageUD"
+                icon={<PersonOutlinedIcon />}
+                selected={selected}
+                setSelected={setSelected}
+              />
               {/* <Item 
                 title="Calendar"
                 to="/calendar"
