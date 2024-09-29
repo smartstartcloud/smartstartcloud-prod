afda41bd266c84d7d7c1e591b0ba2c8e97753f01
farhantech50 <farhantech50@gmail.com>
2024-09-30 02:53:02 +0600

    mixSchema preparations

--- Backend/controllers/module.controller.js
+++ Backend/controllers/module.controller.js
@@ -1,10 +1,13 @@
 import ModuleAssignment from '../models/moduleAssignment.models.js'
 export const newAssignment = async (req,res)=>{
   try{
-    const {studentID,moduleID,assignmentID} = req.body
+    const {studentID,moduleID,orderID} = req.body
     const module = await ModuleAssignment.findOne({moduleID,studentID});
     if (module){
-        await ModuleAssignment.findOneAndUpdate({ _id: module._id },{ $push: { assignmentID: assignmentID } } ).then(result => {
+        if(module.orderID.includes(orderID)){
+          res.status(400).json({value:"Order ID already exists"});
+        }else{
+          await ModuleAssignment.findOneAndUpdate({ _id: module._id },{ $push: { orderID: orderID } } ).then(result => {
             if (result.matchedCount === 0) {
               console.log("No document found with the given _id.");
             } else {
@@ -14,16 +17,32 @@ export const newAssignment = async (req,res)=>{
           .catch(err => {
             console.error("Error updating the document:", err);
           });
+        }
     }else{
         const newAssignment = new ModuleAssignment({
             studentID,
             moduleID,
-            assignmentID,
+            orderID,
           })
           if(newAssignment){
             await newAssignment.save();
             res.status(200).json({value:"Assignment added successfully"});
           }
+    }
+    }catch(error){
+        console.log(error);
+        res.status(500).json({error:"Internal Server Error"});    
+    }
+}
+
+export const getAssignment = async (req,res)=>{
+  try{
+    const {studentID,moduleID} = req.params
+    const module = await ModuleAssignment.findOne({moduleID,studentID});
+    if (module){
+      res.status(200).json(module.orderID);
+    }else{
+        res.status(400).json({value:"No module found"})
     }
     }catch(error){
         console.log(error);
