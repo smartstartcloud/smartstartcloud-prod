eb62ab4ad663c4b88fc9bf76dc6b9a1961cfaf0b
shiammisbah <shiam.misbah42@gmail.com>
2024-09-30 04:25:42 +0600

    Module section added

--- Dashboard/src/components/profilePages/StudentProfile.jsx
+++ Dashboard/src/components/profilePages/StudentProfile.jsx
@@ -1,8 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useLocation, useParams } from 'react-router-dom';
 import useFetchSingleStudentData from '../../hooks/useFetchSingleStudentData';
-import { Box, Card, CardContent, CircularProgress, Divider, Grid, Typography, useTheme } from '@mui/material';
+import { Accordion, AccordionDetails, AccordionSummary, Box, Button, Card, CardContent, CircularProgress, Divider, Grid, IconButton, Modal, Typography, useTheme } from '@mui/material';
 import { tokens } from '../../theme';
+import CloseIcon from '@mui/icons-material/Close';
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+
 
 const StudentProfile = () => {
     const theme = useTheme();
@@ -10,9 +13,17 @@ const StudentProfile = () => {
     const { studentId } = useParams();
     const location = useLocation(); // Access the passed state
     const {student, loading, error} = useFetchSingleStudentData(studentId);
-    const { studentName, studentContact, studentLogin, studentPassword, studentAssignment = [] } = student || {};
+    const { _id, studentName, studentContact, studentLogin, studentPassword, studentAssignment = [] } = student || {};
     const { degreeModules } = location.state || {};
-    const taken = ['204']
+
+    const [open, setOpen] = useState(false);
+    console.log(degreeModules);
+    
+    const taken = ['Module 1', 'Module 3']
+
+    const handleModuleCLick = (item, isValid) => {
+        console.log(item, isValid, _id);
+    }
 
     if (loading) {
         return (
@@ -27,7 +38,7 @@ const StudentProfile = () => {
     }
 
     return (
-        <Box m="20px" display="flex" flexDirection="column" alignItems="center" gap={2}>
+        <Box m="20px auto" display="flex" flexDirection="column" alignItems="center" gap={2} maxWidth='1000px'>
             <Card
                 sx={{
                     width: '100%',
@@ -86,7 +97,7 @@ const StudentProfile = () => {
                                                 key={index}
                                                 variant="body1"
                                                 color={colors.grey[200]}
-                                                sx={{ textAlign: 'center', mb: 1,  textDecoration: taken.includes(module.moduleCode) ? 'none' : 'line-through' }}
+                                                sx={{ textAlign: 'center', mb: 1,  textDecoration: taken.includes(module.moduleName) ? 'none' : 'line-through' }}
                                             >
                                                 {module.moduleName}
                                             </Typography>
@@ -102,7 +113,197 @@ const StudentProfile = () => {
                     </Grid>
                 </CardContent>
             </Card>
+            
+            <Box mt={3}>
+                <Typography variant="h3" color={colors.grey[100]} sx={{ fontWeight: 'bold', mb: 2 }}>
+                    Module List
+                </Typography>
+                <Grid container spacing={2} alignItems='center' width='1000px'>
+                    {degreeModules.map((module)=> (
+                        <Grid key={module._id} item xs={12} sm={3}>
+                            <Card
+                                sx={{
+                                    width: '100%',
+                                    p: 5,
+                                    background: `linear-gradient(145deg, ${taken.includes(module.moduleName) ? colors.blueAccent[700] : colors.redAccent[700]}, ${taken.includes(module.moduleName) ? colors.blueAccent[500] : colors.redAccent[500]})`,
+                                    boxShadow: 6,
+                                    cursor: 'pointer',
+                                    borderRadius: 4,
+                                    '&:hover': {
+                                        boxShadow: 12,
+                                    },
+                                }}
+                                onClick={()=>handleModuleCLick(module._id, taken.includes(module.moduleName))}
+                            >
+                                <Typography variant="h3" color={colors.grey[100]} sx={{ fontWeight: 'bold', textAlign: 'center'}}>
+                                    {module.moduleName}
+                                </Typography>
+                            </Card>
+                        </Grid>
+
+                    ))}
+                    
+                </Grid>
+                {/* <Button
+                    variant="contained"
+                    sx={{
+                        mt: 3,
+                        display: 'block',
+                        mx: 'auto',
+                        backgroundColor: colors.blueAccent[500],
+                        '&:hover': {
+                            backgroundColor: colors.blueAccent[600],
+                        },
+                    }}
+                    onClick={() => setOpen(true)}
+                >
+                    Add Student
+                </Button> */}
+            </Box>
+            {/* <Modal open={open} onClose={() => setOpen(false)}>
+                <Box
+                    sx={{
+                        width: '400px',
+                        backgroundColor: colors.grey[900],
+                        padding: 3,
+                        borderRadius: 3,
+                        mx: 'auto',
+                        mt: '10%',
+                        position: 'relative',
+                    }}
+                >
+                    <IconButton
+                        onClick={() => setOpen(false)}
+                        sx={{ position: 'absolute', top: 10, right: 10, color: colors.grey[50] }}
+                    >
+                        <CloseIcon />
+                    </IconButton>
+                    <Typography variant="h5" color={colors.grey[50]} sx={{ mb: 2 }}>
+                        Add New Student
+                    </Typography>
+                    <TextField
+                        label="Student ID"
+                        fullWidth
+                        error={!!errors.studentID}
+                        helperText={errors.studentID}
+                        value={newStudent.studentID}
+                        onChange={(e) => {
+                            setNewStudent({ ...newStudent, studentID: e.target.value });
+                            setErrors({ ...errors, studentID: '' });
+                        }}
+                        sx={{ mb: 2 }}
+                    />
+                    <TextField
+                        label="Student Name"
+                        fullWidth
+                        error={!!errors.studentName}
+                        helperText={errors.studentName}
+                        value={newStudent.studentName}
+                        onChange={(e) => {
+                            setNewStudent({ ...newStudent, studentName: e.target.value });
+                            setErrors({ ...errors, studentName: '' });
+                        }}
+                        sx={{ mb: 2 }}
+                    />
+                    <TextField
+                        label="Student Login"
+                        fullWidth
+                        error={!!errors.studentLogin}
+                        helperText={errors.studentLogin}
+                        value={newStudent.studentLogin}
+                        onChange={(e) => {
+                            setNewStudent({ ...newStudent, studentLogin: e.target.value });
+                            setErrors({ ...errors, studentLogin: '' });
+                        }}
+                        sx={{ mb: 2 }}
+                    />
+                    <TextField
+                        label="Student Password"
+                        fullWidth
+                        error={!!errors.studentPassword}
+                        helperText={errors.studentPassword}
+                        value={newStudent.studentPassword}
+                        onChange={(e) => {
+                            setNewStudent({ ...newStudent, studentPassword: e.target.value });
+                            setErrors({ ...errors, studentPassword: '' });
+                        }}
+                        sx={{ mb: 2 }}
+                    />
+                    <TextField
+                        label="Student Contact"
+                        fullWidth
+                        error={!!errors.studentContact}
+                        helperText={errors.studentContact}
+                        value={newStudent.studentContact}
+                        onChange={(e) => {
+                            setNewStudent({ ...newStudent, studentContact: e.target.value });
+                            setErrors({ ...errors, studentContact: '' });
+                        }}
+                        sx={{ mb: 2 }}
+                    />
+                    <Button
+                        variant="contained"
+                        onClick={handleSubmit}
+                        sx={{
+                            width: '100%',
+                            backgroundColor: colors.blueAccent[500],
+                            '&:hover': {
+                                backgroundColor: colors.blueAccent[600],
+                            },
+                        }}
+                    >
+                        Add
+                    </Button>
+                </Box>
+            </Modal> */}
+            <Box mt={3} width='100%'>
+                <Typography variant="h3" color={colors.grey[100]} sx={{ fontWeight: 'bold', mb: 2}}>
+                    Assignment List for Module 1
+                </Typography>
+                <Grid container spacing={2} width='100%' mt={2}>
+                    <Accordion sx={{width: '100%', background: colors.grey[900], color: colors.grey[200]}}>
+                        <AccordionSummary
+                        expandIcon={<ArrowDropDownIcon sx={{color: colors.grey[200]}} />}
+                        aria-controls="panel2-content"
+                        id="panel2-header"
+                        >
+                            <Grid item xs={6} sm={3} >
+                                <Typography>
+                                    Academic Skills for Business Management 
+                                </Typography>
+                            </Grid>
+                            <Grid item xs={6} sm={3} >
+                                <Typography>
+                                    Group Presentation 
+                                </Typography>
+                            </Grid>
+                            <Grid item xs={6} sm={3} >
+                                <Typography>
+                                    27/02/23
+                                </Typography>
+                            </Grid>
+                            <Grid item xs={6} sm={2} >
+                                <Typography>
+                                    55%
+                                </Typography>
+                            </Grid>
+                            <Grid item xs={6} sm={1} >
+                                <Typography>
+                                    PAID
+                                </Typography>
+                            </Grid>
+                        </AccordionSummary>
+                        <AccordionDetails sx={{background: colors.grey[800], color: colors.grey[200]}}>
+                        <Typography>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+                            malesuada lacus ex, sit amet blandit leo lobortis eget.
+                        </Typography>
+                        </AccordionDetails>
+                    </Accordion>
+                </Grid>
+            </Box>
         </Box>
+        
 
     )
 }
