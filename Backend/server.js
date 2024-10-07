c0e361b9dcfe6cd864fd303203573a80f57bd74c
farhantech50 <farhantech50@gmail.com>
2024-10-08 04:31:15 +0600

    u

--- Backend/server.js
+++ Backend/server.js
@@ -34,6 +34,15 @@ const app = express();
 //   });
 // };
 
+
+/*
+//To deploy Frontend and Backend in save Heroku App
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+app.use(express.static(path.join(__dirname,'Dashboard/build'))); //To connect react app
+*/
+
+
 // Middleware
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
