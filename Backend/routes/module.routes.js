9175fc56bcc22e6d980fe45efee200055a59d6c6
shiammisbah <shiam.misbah42@gmail.com>
2024-11-13 01:39:20 +0600

    dir updated

--- Backend/routes/module.routes.js
+++ Backend/routes/module.routes.js
@@ -2,7 +2,7 @@ import express from "express"
 import {newAssignment,getAssignment} from '../controllers/module.controller.js'
 const router = express.Router();
 import {authenticate} from '../middlewares/protect.js'
-import { deleteAssignment, newAssignmentManual, updateAssignment } from "../controllers/assignment.controllers.js";
+import { deleteAssignment, newAssignmentManual, updateAssignment } from "../controllers/assignment.controller.js";
 
 router.use(authenticate);
 router.post("/newAssignment",newAssignmentManual);
