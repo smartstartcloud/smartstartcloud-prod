5a9df7f6cdefa612df8827a077e69f6a0a271002
Shiam-Sebpo <shiam.misbah42@gmail.com>
2025-02-19 19:14:45 +0600

    Update useLogout.js

--- Dashboard/src/hooks/useLogout.js
+++ Dashboard/src/hooks/useLogout.js
@@ -1,18 +1,19 @@
 import { useNavigate } from 'react-router-dom';
 import { useAuthContext } from "../context/AuthContext";
 import { useTokenContext } from '../context/TokenContext';
-import useApi from './useApi';
+import axios from 'axios';
+import { switchBackendURL } from '../utils/connections';
 
 const useLogout = () => {
-  const api = useApi()
   const { setAuthUser } = useAuthContext(); // To update the authenticated user state
   const { setAccessToken } = useTokenContext(); // To update the access token state
   const navigate = useNavigate();
+  const baseURL = switchBackendURL()
 
   const logout = async () => {
     try {
       // Send a logout request to the backend
-      const res = await api.post(`/api/auth/logout`, {});
+      const res = await axios.post(`${baseURL}/api/auth/logout`, {});
 
       const data = await res.data;
       console.log(data);
